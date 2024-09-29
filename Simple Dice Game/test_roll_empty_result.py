4d7e88bad13744e26cf5b243776bca8f55a35624
Macky <michaelmontana0822@gmail.com>
2024-09-29 21:39:27 +0800

    Update test_roll_empty_result.py

--- Simple Dice Game/test_roll_empty_result.py
+++ Simple Dice Game/test_roll_empty_result.py
@@ -1,6 +1,7 @@
 import pytest
 from simple_dice_game import roll
 
+
 def test_roll_empty_result(): # Ensures that after three invalid combinations, the function returns an empty list.
     for _ in range(1000):
         result = roll()
